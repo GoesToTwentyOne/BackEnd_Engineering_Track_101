da60928039427801e2ae8571e5fbcdadd37e0024
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-06-19 17:59:28 +0600

    done

---
+++ 01_OOP_AND_PYTHON_PROGRAMMING/WEEK_001_INTRODUCTION_TO_PYTHON/Lecture_03/immutable_string.py
@@ -0,0 +1,14 @@
+name="Md. Nihad Hossain"
+name1='Joy Bangla\'s gan'
+name2=""" 
+Life is self peaced learning
+don't compare it others
+"""
+print(name)
+print(name1)
+
+if 'Joy' in name1:
+    print("Yes")
+
+x=r'D:\nsources\sources'
+print(x)
