5efa1cb55db8be6350beafadbb534ec3b01a1c73
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-06-19 20:11:45 +0600

    done

---
+++ 01_OOP_AND_PYTHON_PROGRAMMING/WEEK_001_INTRODUCTION_TO_PYTHON/Lecture_03/python_modules.py
@@ -0,0 +1,8 @@
+from math import ceil
+from random import randint
+from random import choice
+from datetime import *
+print(ceil(45.2))
+print(randint(1,100))
+print(choice(['Nihad',"Alex",'Goot','Apinkar']))
+print(date.today())
