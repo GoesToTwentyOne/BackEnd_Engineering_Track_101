6e9f9f6708ae26602e4ff5c80c3153ca62fbd62e
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-07-18 23:36:14 +0600

    done

---
+++ 02_DATABASE_MANAGEMENT_SYSTEM/WEEK_04_PYTHON_WITH_MYSQL/02_Operations_execute_by_python/insert_values_into_table.py
@@ -0,0 +1,24 @@
+import mysql.connector
+
+my_db=mysql.connector.connect(
+    user="nihadgo75",
+    host="localhost",
+    passwd="nihadgo75",
+    database="python_with_mysql",
+)
+
+my_cursor=my_db.cursor()
+
+sql_command="""
+INSERT INTO student(Name,Roll,Class) 
+VALUES("A","101","seven"),
+    ("B","102","Eight"),
+    ("C","103","Nine"),
+    ("D","104","Eleven"),
+    ("E","105","Twelve");
+"""
+my_cursor.execute(sql_command)
+
+my_cursor.execute("SELECT * FROM student;")
+data=my_cursor.fetchall()
+print(data)
