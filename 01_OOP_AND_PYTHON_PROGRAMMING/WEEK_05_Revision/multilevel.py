33abf898dc940c3bba71d57c5e20ac06e7ac9d0c
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-08-18 15:45:04 +0600

    done

--- 01_OOP_AND_PYTHON_PROGRAMMING/WEEK_05_Revision/multilevel.py
+++ 01_OOP_AND_PYTHON_PROGRAMMING/WEEK_05_Revision/multilevel.py
@@ -25,7 +25,7 @@ class Phone(Device):
     def __repr__(self) -> str:
         return super().__repr__() + f" {self.is_sim} {self.is_callback}"
 
-class Laptop(Device):
+class Laptop(Device):  # Changed inheritance to Device
     def __init__(self, model, color, brand, device_type, config, speed_benchmark):
         self.config = config
         self.speed_benchmark = speed_benchmark
@@ -37,11 +37,11 @@ class Laptop(Device):
     def __repr__(self) -> str:
         return super().__repr__() + f" {self.config} {self.speed_benchmark}"
 
-class Tab(Device):
+class Tab(Laptop):  # Changed inheritance to Laptop
     def __init__(self, model, color, brand, device_type, s_size, charging_capacity):
         self.s_size = s_size
         self.charging_capacity = charging_capacity
-        super().__init__(model, color, brand, device_type)
+        super().__init__(model, color, brand, device_type, config=None, speed_benchmark=None)  # Calling parent constructor
 
     def reg(self):
         print("Reg is on Tab")
@@ -49,16 +49,7 @@ class Tab(Device):
     def __repr__(self) -> str:
         return super().__repr__() + f" {self.s_size} {self.charging_capacity}"
 
-startechPhone = Phone(model='M31 Prime', color='Green', brand='Samsung', device_type='Mobile', is_sim=True, is_callback=True)
-startechLaptop = Laptop(model='K513 EA', color='Red', brand='Asus', device_type='Laptop', config='High Conf.', speed_benchmark='High Speed Tested')
 startechTab = Tab(model='H87', color='Red', brand='Samsung', device_type='Tab', s_size="7.9", charging_capacity="78000")
-
-# Print attributes and values
-for att_name, att_value in vars(startechPhone).items():
-    print(att_name, att_value)
-
-for att_name, att_value in vars(startechLaptop).items():
-    print(att_name, att_value)
 
 for att_name, att_value in vars(startechTab).items():
     print(att_name, att_value)
