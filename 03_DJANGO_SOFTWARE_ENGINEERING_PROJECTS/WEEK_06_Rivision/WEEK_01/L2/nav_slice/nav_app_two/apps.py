35bae1fdcb6bfc3b19993dae402d2021b0ff292e
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-12-09 09:42:10 +0600

    done

--- 03_DJANGO_SOFTWARE_ENGINEERING_PROJECTS/WEEK_06_Rivision/WEEK_01/L2/nav_slice/nav_app_two/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NavAppTwoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'nav_app_two'
--- 03_DJANGO_SOFTWARE_ENGINEERING_PROJECTS/WEEK_06_Rivision/WEEK_01/L2/nav_slice/nav_app_two/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from nav_app_two.views import two_home,two_course
-urlpatterns = [
-    path('two_home/',two_home,name='two_home'),
-    path('two_contact/',two_course,name='two_course'),
-]
