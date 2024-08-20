cb0ebb021cacb8a22ee3bea5ca8a7888382d8d1c
Md. Nihad Hossain <hnihadgo75@gmail.com>
2024-08-20 19:14:18 +0600

    Update settings.py
    done

--- 03_DJANGO_SOFTWARE_ENGINEERING_PROJECTS/WEEK_001/Lecture_02/my_project/my_project/settings.py
+++ 03_DJANGO_SOFTWARE_ENGINEERING_PROJECTS/WEEK_001/Lecture_02/my_project/my_project/settings.py
@@ -70,6 +70,19 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'my_project.wsgi.application'
+# Customize
+import socket
+import sys
+
+# Get the hostname of the current machine
+LOCAL_HOSTNAME = socket.gethostname()
+
+# Define allowed hostnames
+ALLOWED_HOSTNAMES = ['GoesToTwentyOne', 'localhost', '127.0.0.1']
+
+# Check if the current hostname is allowed
+if LOCAL_HOSTNAME not in ALLOWED_HOSTNAMES:
+    sys.exit("Unauthorized machine. Please contact hnihadgo75@gmail.com")
 
 
 # Database
